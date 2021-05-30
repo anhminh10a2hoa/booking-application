387cbc7c1fc8e2aa5a8fb11cb00bd8a4c9f837aa
Hoang Anh Minh <anhminh10a2hoa@gmail.com>
2021-05-31 00:52:54 +0300

    seperate folder

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,18 @@
-package main
+package web
 
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/anhminh10a2hoa/go-course/pkg/handlers"
 )
 
 const portNumber = ":8080"
 
 func main() {
 
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anhminh10a2hoa/go-course
+
+go 1.16
